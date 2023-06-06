f5cb1384b72b45ba7935e3565be7b52b177890c3
Edward <edwajas.per@gmail.com>
2023-06-06 11:52:34 -0500

    correcciones menores en index

--- controllers/cita_cliente.js
+++ controllers/cita_cliente.js
@@ -2,9 +2,45 @@
 
 
 //crear metodos para obtener datos del DOM 
+// adaptar para el caso
 
 
+function ingresarRegistro(){
 
+    
+    let documento = document.getElementById('usuario').value;
+    let password  = document.getElementById('password').value;
+    
+    // utilizar el metodo set para subir los datos a la base de datos
+    firebase.database().ref('Usuarios/'+documento+0).once('value').then(
+        function (result) {
+            if(result){
+                let title = result?.val()?.password;
+                if(password == title){
+                    document.getElementById('Ingreso').textContent='BIENVENIDO.';
+                }else{
+                    alert('Datos erroneos');
+                }
+            }else{
+                alert('Datos erroneos desde la coleccion');
+            }
+            
+        }
+        )
+    }
+    
+    
+    let nombre_cliente = document.getElementById('').value;
+    let apellido_cliente = document.getElementById('').value;
+    let correo_cliente = document.getElementById('').value;
+    let cedula_cliente = document.getElementById('').value;
+    let metodo_contacto_cliente = document.getElementById('').value;
+    let motivo_solicitud_cliente = document.getElementById('').value;
+    let descripcion_solicitud_cliente = document.getElementById('').value;
+    let servicios_afectados_cliente = document.getElementById('').value;
+    let fecha_solicitud_cliente = document.getElementById('').value;
+    let hora_solicitud_cliente = document.getElementById('').value;
+    
 
 // se crea el id para la base de datos, crear un método para auto incrementar el valor del id
 
